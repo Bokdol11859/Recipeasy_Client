0a2cdc5d92e12f3249814dc6db16b70321a9675e
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-01-09 18:25:26 +0900

    Feat: Implement Login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,45 @@
+import styled from '@emotion/styled';
+import { KakaoLogin } from '@src/components/icons/SocialIcons';
+import COLOR from '@src/constants/theme';
+import Image from 'next/image';
 import React from 'react';
 
-type Props = {};
-
-const Login = (props: Props) => {
-  return <div>Login</div>;
+const Login = () => {
+  return (
+    <Container>
+      <Logo>
+        <Image src="/assets/LoginLogo.png" alt="Login Logo" width={220} height={90} />
+      </Logo>
+      <Footer>
+        <KakaoLogin onClick={() => {}} />
+      </Footer>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  /* background-color: ${COLOR.BACKGROUND}; */
+`;
+
+const Logo = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+
+const Footer = styled.div`
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default Login;
