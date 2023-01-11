c07c07f351b65da5b13d03f3b363c8286c772015
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-01-11 18:13:06 +0900

    Feat: Set up axios instance

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
 import type { AppProps } from 'next/app';
 import Layout from '../components/global/Layout';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import '../../styles/globals.css';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   );
 }
