7426f7fd3d3faeb9a1f56bbd8cda1bbd06fb767d
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-02-02 21:23:59 +0900

    Fix: Fix max width issue on small card

--- src/components/global/Cards.tsx
+++ src/components/global/Cards.tsx
@@ -24,7 +24,7 @@ export const SmallCard = ({
         priority
         width={400}
         height={500}
-        className="h-auto w-[42vw] max-w-[200px] rounded-xl bg-cover brightness-100"
+        className="h-auto w-[42vw] max-w-[190px] rounded-xl bg-cover brightness-100"
         src={image}
         alt={title}
       />
@@ -39,7 +39,7 @@ export const SmallCard = ({
 
 export const LoadingSmallCard = () => (
   <div className="relative flex flex-col">
-    <div className="h-[300px] w-[42vw] max-w-[200px] animate-pulse rounded-xl bg-gray-300" />
+    <div className="h-[300px] w-[42vw] max-w-[190px] animate-pulse rounded-xl bg-gray-300" />
     <div className="mt-1 ml-1 h-4 w-[75%] animate-pulse bg-gray-300" />
   </div>
 );
